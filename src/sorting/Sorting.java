4d7e5342fec3520bae3eea04b36a5b1e32bd07b5
Donal Devine <5518040+donaldevine@users.noreply.github.com>
2023-09-17 15:10:27 +0100

    quick sort

--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -4,34 +4,68 @@ public class Sorting {
 	
 	public static void main(String args[]) {
 		
-		int insertionSortArr[] = {12, 11, 13, 5, 6};		
-		InsertionSort insertionSort = new InsertionSort();		
-		System.out.println("Insertion Sort: ");
+		int insertionSortArr[] = {12, 11, 13, 5, 6};
+		
+		System.out.println("Before Insertion Sort: ");
+		printArray(insertionSortArr);
+		
+		InsertionSort insertionSort = new InsertionSort();
 		insertionSort.sort(insertionSortArr);		
+		
+		System.out.println("After Insertion Sort: ");
 		printArray(insertionSortArr);
 		
         
-		SelectionSort selectionSort = new SelectionSort();        
-		int selectionSortArr[] = {64,25,12,22,11};		
+		int selectionSortArr[] = {64,25,12,22,11};
+		
+		System.out.println("Before Selection Sort: ");
+        printArray(selectionSortArr);
+		
+		SelectionSort selectionSort = new SelectionSort();
+						
 		selectionSort.sort(selectionSortArr);
-        System.out.println("Selection Sort: ");
+		
+        System.out.println("After Selection Sort: ");
         printArray(selectionSortArr);
         
         
-        int bubbleSortArr[] = { 64, 34, 25, 12, 22, 11, 90 };        
+        int bubbleSortArr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        
+        System.out.println("Before Bubble Sort: ");
+        printArray(bubbleSortArr);
+        
         BubbleSort bubbleSort = new BubbleSort();        
         bubbleSort.sort(bubbleSortArr);        
-        System.out.println("Bubble Sort: ");
+        
+        System.out.println("After Bubble Sort: ");
         printArray(bubbleSortArr);
         
         
         int mergeSortArr[] = { 12, 11, 13, 5, 6, 7 };
+        
+        System.out.println("Before Merge Sort: ");
+        printArray(mergeSortArr);
                  
         MergeSort mergeSort = new MergeSort();
         mergeSort.sort(mergeSortArr, 0, mergeSortArr.length - 1);
  
-        System.out.println("Merge Sort: ");
+        System.out.println("After Merge Sort: ");
         printArray(mergeSortArr);
+        
+        
+        int[] quickSortArray = { 10, 7, 8, 9, 1, 5 };
+        
+        System.out.println("Before Quick Sort: ");
+        printArray(quickSortArray);
+        
+        int N = quickSortArray.length;
+        
+        QuickSort quickSort = new QuickSort();
+        
+        quickSort.sort(quickSortArray, 0, N - 1);
+                
+        System.out.println("After Quick Sort: ");
+        printArray(quickSortArray);
 		
 	}
 	
@@ -46,6 +80,7 @@ public class Sorting {
 		}
 		
 		
+		System.out.println();
 		System.out.println();
 		
 	}
